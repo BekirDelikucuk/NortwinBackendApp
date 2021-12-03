56c696420f6e2e5ab3be434104feb5aa3bc3300b
BekirDelikucuk <91602566+BekirDelikucuk@users.noreply.github.com>
2021-12-03 23:12:54 +0300

    JWT ve Autofac İmplementation
    rol bazlı yapı , operations success ,
    success and failure management

---
+++ NorthwindBackend/Business/Concrete/AuthManager.cs
@@ -0,0 +1,76 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Hashing;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private IUserService _userService;
+        private ITokenHelper _tokenHelper;
+
+
+
+        public AuthManager(IUserService userService, ITokenHelper tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            var claims = _userService.GetClaims(user);
+            var accesstoken = _tokenHelper.CreateToken(user, claims);
+            return new SuccessDataResult<AccessToken>(accesstoken, Messages.AccessTokenCreated);
+               
+        }
+
+        public IDataResult<User> Login(UserLoginDto userLoginDto)
+        {
+            var UserToCheck = _userService.GetByMail(userLoginDto.Email);
+            if (UserToCheck==null)
+            {
+                return new ErrorDataResult<User>(Messages.UserNotFound);
+            }
+            if (!HashingHelper.VerifyPasswordHash(userLoginDto.Password,UserToCheck.PasswordHash,UserToCheck.PasswordSalt))
+            {
+                return new ErrorDataResult<User>(Messages.PasswordError);
+            }
+            return new SuccessDataResult<User>(UserToCheck, Messages.SuccessfulLogin);
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            var user = new User
+            {
+                Email =userForRegisterDto.Email,
+                FirstName=userForRegisterDto.FirstName,
+                LastName=userForRegisterDto.LastName,
+                PasswordHash=passwordHash,
+                PasswordSalt=passwordSalt,
+                Status=true
+
+            };
+            _userService.Add(user);
+            return new SuccessDataResult<User>(user, Messages.UserRegistered);
+        }
+
+        public IResult UserExist(string email)
+        {
+            if (_userService.GetByMail(email)!=null)
+            {
+                return new ErrorResult(Messages.UserAlreadyExist);
+            }
+            return new SuccessResult();
+        }
+    }
+}
---
+++ NorthwindBackend/Business/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarıyla eklendi";
+        public static string ProductDeleted = "Ürün başarıyla silind";
+        public static string ProductUptaded = "Ürün başarıyla güncellendi";
+
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Kullanıcı başarıyla sisteme giriş yaptı";
+        public static string UserAlreadyExist = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access Token başarıyla oluşturuldu";
+    }
+}
